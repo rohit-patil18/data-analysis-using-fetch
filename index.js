d7da014db035b109756f3ca37ff7aec1ff74d9e2
rohit-patil18 <rohit.patil1@somaiya.edu>
2023-10-25 20:35:38 +0530

    data analysed

--- index.js
+++ index.js
@@ -1,18 +1,75 @@
-const baseUrl = "https://one00x-data-analysis.onrender.com";
-
-let assignmentId;
+const baseUrl = "https://one00x-data-analysis.onrender.com/assignment";
 
 const getData = async () => {
-    const response = await fetch(`${baseUrl}/assignment?email=engineer.rohit18patil@gmail.com`, {
-        headers: {
-            'x-assignment-id': assignmentId
-        },
-        method: 'GET',
-    });
-    // console.log({ response });
-    const data = await response.json();
-    console.log({ data: response.headers.get('x-assignment-id') });
-    return data;
+	let response;
+	let data;
+
+	try {
+		response = await fetch(`${baseUrl}?email=engineer.rohit18patil@gmail.com`);
+		data = await response.json();
+	} catch (err) {
+		console.error("Error: " + err.message);
+		return null;
+	}
+
+	const assignmentId = response.headers.get('x-assignment-id');
+
+	return {
+		assignmentId,
+		data,
+	}
 };
 
-getData();
+const submitData = async (assignmentId, mostUsedJargons) => {
+	let res;
+
+	for (const jargon of mostUsedJargons) {
+		try {
+			res = await fetch(baseUrl, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					assignment_id: assignmentId,
+					answer: jargon,
+				}),
+			})
+		} catch {
+			console.error('Failed to submit the data');
+		}
+
+		const data = await res.json();
+
+		console.log(`Response for the jargon "${jargon}":`);
+		console.log(data);
+	}
+}
+
+const analyseData = async () => {
+	const { data = '', assignmentId = '' } = await getData();
+
+	let marketingTermsObject = {};
+
+	data.forEach((item) => {
+		if (marketingTermsObject.hasOwnProperty(item)) {
+			marketingTermsObject[item] += 1;
+		} else {
+			marketingTermsObject[item] = 1;
+		}
+	})
+
+	const termUsedMaxTimes = Math.max(...Object.values(marketingTermsObject));
+
+	let mostUsedJargons = [];
+
+	for (const [phrase, phraseCount] of Object.entries(marketingTermsObject)) {
+		if (phraseCount === termUsedMaxTimes) {
+			mostUsedJargons.push(phrase);
+		}
+	}
+
+	await submitData(assignmentId, mostUsedJargons);
+}
+
+analyseData();
